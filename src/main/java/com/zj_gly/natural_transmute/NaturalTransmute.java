39336edf4ca5ed21cde5a3464d53f4f5186aba9d
glyceryl <494192175@qq.com>
2024-08-02 10:10:39 +0800

    Initial commit

---
+++ src/main/java/com/zj_gly/natural_transmute/NaturalTransmute.java
@@ -0,0 +1,23 @@
+package com.zj_gly.natural_transmute;
+
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+import java.util.Locale;
+
+@Mod(NaturalTransmute.MOD_ID)
+public class NaturalTransmute {
+
+    public static final String MOD_ID = "natural_transmute";
+
+    public NaturalTransmute(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+
+    public static ResourceLocation prefix(String name) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, name.toLowerCase(Locale.ROOT));
+    }
+
+}
