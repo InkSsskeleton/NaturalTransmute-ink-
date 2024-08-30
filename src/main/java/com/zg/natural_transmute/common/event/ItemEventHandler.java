414b9eeb12ec7f89bc548dc12f597c74a0cb4985
glyceryl <494192175@qq.com>
2024-08-30 17:24:53 +0800

    赋予“蠹虫蛹”酿造“虫蚀药水”的特性。

--- src/main/java/com/zg/natural_transmute/common/event/ItemEventHandler.java
+++ src/main/java/com/zg/natural_transmute/common/event/ItemEventHandler.java
@@ -3,15 +3,23 @@ package com.zg.natural_transmute.common.event;
 import com.zg.natural_transmute.NaturalTransmute;
 import com.zg.natural_transmute.common.components.AssociatedBiomes;
 import com.zg.natural_transmute.registry.NTDataComponents;
+import com.zg.natural_transmute.registry.NTItems;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.alchemy.Potions;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.brewing.RegisterBrewingRecipesEvent;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 import java.util.List;
 
 @EventBusSubscriber(modid = NaturalTransmute.MOD_ID)
 public class ItemEventHandler {
+
+    @SubscribeEvent
+    public static void onRegisterBrewingRecipes(RegisterBrewingRecipesEvent event) {
+        event.getBuilder().addStartMix(NTItems.SILVERFISH_PUPA.get(), Potions.INFESTED);
+    }
 
     @SubscribeEvent
     public static void onItemTooltips(ItemTooltipEvent event) {
