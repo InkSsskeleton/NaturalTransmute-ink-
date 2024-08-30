4a071f4daba39f995325ea02230779b18b455839
glyceryl <494192175@qq.com>
2024-08-30 16:43:06 +0800

    将创造模式物品栏的图标改成聚相台。

--- src/main/java/com/zg/natural_transmute/registry/NTCreativeModeTabs.java
+++ src/main/java/com/zg/natural_transmute/registry/NTCreativeModeTabs.java
@@ -4,6 +4,7 @@ import com.zg.natural_transmute.NaturalTransmute;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -11,7 +12,7 @@ public class NTCreativeModeTabs {
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, NaturalTransmute.MOD_ID);
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> NORMAL = CREATIVE_MODE_TABS.register("normal", () -> CreativeModeTab
-            .builder().title(Component.translatable("itemGroup." + NaturalTransmute.MOD_ID)).icon(() -> NTItems.H_BADLANDS.get().getDefaultInstance())
+            .builder().title(Component.translatable("itemGroup." + NaturalTransmute.MOD_ID)).icon(() -> new ItemStack(NTItems.GATHERING_PLATFORM))
             .displayItems((parameters, output) -> NTItems.ITEMS.getEntries().forEach(holder -> output.accept(holder.get()))).build());
 
 }
