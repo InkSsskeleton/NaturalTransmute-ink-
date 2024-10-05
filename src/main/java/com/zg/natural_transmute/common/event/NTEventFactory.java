ffba3c4a261679056402235af4394d82101e926f
glyceryl <494192175@qq.com>
2024-10-05 18:43:32 +0800

    添加一个用于注册特殊缚相的事件接口。

---
+++ src/main/java/com/zg/natural_transmute/common/event/NTEventFactory.java
@@ -0,0 +1,15 @@
+package com.zg.natural_transmute.common.event;
+
+import com.zg.natural_transmute.common.blocks.entity.GatheringPlatformBlockEntity;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.common.NeoForge;
+
+import java.util.Map;
+
+public class NTEventFactory {
+
+    public static void onRegisterSpecialFuXiangCraftingCondition(Map<Item, Boolean> conditionMaps, GatheringPlatformBlockEntity blockEntity) {
+        NeoForge.EVENT_BUS.post(new RegisterSpecialFuXiangCraftingConditionEvent(conditionMaps, blockEntity));
+    }
+
+}
